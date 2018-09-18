856f5607c0edca3954d1edd11276bf6f28259eea
Jenny Liang <jennylove804@gmail.com>
2018-09-18 17:40:42 -0400

    initial

---
+++ app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyDx0Kzi7HOcfsa5AMYu_DmNSYZSKKICPZY",
+    authDomain: "coding-bootcamp-4f836.firebaseapp.com",
+    databaseURL: "https://coding-bootcamp-4f836.firebaseio.com",
+    projectId: "coding-bootcamp-4f836",
+    storageBucket: "coding-bootcamp-4f836.appspot.com",
+    messagingSenderId: "38139518510"
+  };
+  firebase.initializeApp(config);
